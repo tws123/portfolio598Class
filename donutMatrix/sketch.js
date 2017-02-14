1704539284ef0bd5dca41e2b2d12d753fd4598c5
tws123 <tesstify@gmail.com>
2017-02-14 12:56:19 -0800

    updated w/ more  comments

--- donutMatrix/sketch.js
+++ donutMatrix/sketch.js
@@ -21,81 +21,78 @@ function setup() {
   background(200);
   createCanvas(850, 500, WEBGL);
   // textFont("Work Sans"). No 2D text available w/ WEBGL at this time
-
 }
 
 function draw() {
   background(212, 232, 85); // chartreuse
-  // textTop();
   ambientLight(255, 0, 0); // red
+  light += 0.05; // speed of directional light movement
   directionalLight(255, 100, 0, 0.1, -1, 0, sin(light / 2)); // (green, blue, opacity, x, y, z)
   directionalLight(0, 0, 200, 0.1, 1, 0, sin(light)); // (green, blue, opacity, x, y, z)
-  light += 0.025;
-  translate(-325, -155, 0); // moves object from center to corner
+
+  translate(-325, -155, 0); // moves object from default center to corner
   if (page == 0) {
-    for (i = 0; i < 5; i++) { // donuts along x
-      for (j = 0; j < 3; j++) { // donuts along y
-        push(); // new shift per each donut
-        translate(i * xTrans, j * yTrans, zTrans); // draws donut in new location
-        push(); // separate rotation per donut
-        rotateX(frameCount * 0.01);
-        torus(donutSize, donutSize / 2); // radius, size of whole
+    for (i = 0; i < 5; i++) { // draws torus along x
+      for (j = 0; j < 3; j++) { // draws torus along y
+        push(); // new shift per each torus
+        translate(i * xTrans, j * yTrans, zTrans); // draws torus in new location
+        push(); // separate rotation per torus
+        rotateX(frameCount * 0.01); // rotates along x axis
+        torus(donutSize, donutSize / 2); // draws torus (radius, size of hole)
         pop(); // resets rotation
         pop(); // resets translation
       }
     }
   } else if (page == 1) {
-    for (i = 0; i < 5; i++) { // donuts along x
-      for (j = 0; j < 3; j++) { // donuts along y
-        push(); // new shift per donut
-        translate(i * xTrans, j * yTrans, zTrans); // draws donut in new location
-        push(); // separate rotation per donut
-        rotateY(frameCount * 0.01); // rotates donuts along y axis
-        torus(donutSize, donutSize / 2); // radius, size of whole
+    for (i = 0; i < 5; i++) { // draws torus along x
+      for (j = 0; j < 3; j++) { // draws torus along y
+        push(); // new shift per torus
+        translate(i * xTrans, j * yTrans, zTrans); // draws torus in new location
+        push(); // separate rotation per torus
+        rotateY(frameCount * 0.01); // rotates torus along y axis
+        torus(donutSize, donutSize / 2); // draws torus (radius, size of hole)
         pop(); // resets rotation
         pop(); // resets translation
       }
     }
   } else if (page == 2) {
-    for (i = 0; i < 5; i++) { // donuts along x
-      for (j = 0; j < 3; j++) { // donuts along y
-        push(); // new shift per donut
-        translate(i * xTrans, j * yTrans, zTrans); // draws donut in new location
-        push(); // separate rotation per donut
-        rotateZ(frameCount * 0.01);
-        rotateX(frameCount * 0.01);
-        rotateY(frameCount * 0.01); // rotates donuts along x axis
-        torus(donutSize, donutSize / 2); // radius, size of whole
+    for (i = 0; i < 5; i++) { // draws torus along x
+      for (j = 0; j < 3; j++) { // draws torus along y
+        push(); // new shift per torus
+        translate(i * xTrans, j * yTrans, zTrans); // draws taurus in new location
+        push(); // separate rotation per torus
+        rotateZ(frameCount * 0.01); // rotates torus along z axis
+        rotateX(frameCount * 0.01); // rotates torus along x axis
+        rotateY(frameCount * 0.01); // rotates torus along y axis
+        torus(donutSize, donutSize / 2); // draws torus (radius, size of hole)
         pop(); // resets rotation
         pop(); // resets translation
       }
     }
-  } else if (page == 5) {
-    for (i = 0; i < 5; i++) { // donuts along x
-      for (j = 0; j < 3; j++) { // donuts along y
-        translate(+325, +155, 0); // moves object from center to corner
-        translate(i * xTrans, j * yTrans, zTrans - 300); // draws donut in new location
-        rotateZ(frameCount * 0.01);
-        torus(donutSize, donutSize / 2); // radius, size of whole
-      }
-    }
-  } else if (page == 4) {
-    for (i = 0; i < 5; i++) { // donuts along x
-      for (j = 0; j < 3; j++) { // donuts along y
-        translate(+325, +155, 0); // moves object from center to corner
-        //translate(i * xTrans, j * yTrans, zTrans - 300); // draws donut in new location
-        rotateZ(frameCount * 0.01);
-        torus(donutSize, donutSize / 2); // radius, size of whole
-      }
-    }
   } else if (page == 3) {
-    translate(+325, +155, 0); // moves object from center to corner
-    rotateX(frameCount * 0.01);
-    torus(donutSize * 3.5, (donutSize * 3.5) / 3.5); // radius, size of whole
-    rotateY(frameCount * 0.01); // rotates donuts along x axis
-    //rotateZ(frameCount * 0.01);
-    torus(donutSize * 1.8, (donutSize * 1.8) / 2.8); // radius, size of whole
-    sphere(donutSize);
+    translate(+325, +155, 0); // moves objects back from top/L starting point to default center
+    rotateX(frameCount * 0.01); // rotates exterior torus along x
+    torus(donutSize * 3.5, (donutSize * 3.5) / 3.5); // draws largest torus (radius, size of hole)
+    rotateY(frameCount * 0.01); // rotates torus along x axis
+    torus(donutSize * 1.8, (donutSize * 1.8) / 2.8); // draws smaller torus (radius, size of hole)
+    sphere(donutSize); // draws sphere in torus
+  } else if (page == 4) {
+    for (i = 0; i < 5; i++) { // torus along x
+      for (j = 0; j < 3; j++) { // torus along y
+        translate(+325, +155, 0); // moves starting objects back from top/L starting point to default center
+        rotateZ(frameCount * 0.01);
+        torus(donutSize, donutSize / 2); // draws torus (radius, size of hole)
+      }
+    }
+  } else if (page == 5) {
+    for (i = 0; i < 5; i++) { // sphere along x
+      for (j = 0; j < 3; j++) { // sphere along y
+        translate(+325, +155, 0); // moves starting point back to center 
+        translate(i * xTrans, (j * yTrans)/10, zTrans - 300); // draws sphere in new location
+        rotateZ(frameCount * 0.01);
+        sphere(donutSize); // radius sphere
+      }
+    }
   }
 }
 
